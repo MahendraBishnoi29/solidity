79c716fddc0818ec4946aaac0e7fa3d3ef679b19
"Mahendra" <"mahendrabrowny@gmai.com">
2022-06-07 17:26:03 +0530

    learned about nested mappings

--- Mappings.sol
+++ Mappings.sol
@@ -4,10 +4,17 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract Mapping{
  
-   mapping(uint => string) public myCity;
+//    mapping(uint => string) public myCity;
 
-    function setHouse(uint _hs, string memory _hn) public{
-         myCity[_hs] = _hn;
-    }
+//     function setHouse(uint _hs, string memory _hn) public{
+//          myCity[_hs] = _hn;
+//     }
+// another example
+            
+            mapping(address => mapping(uint => bool)) public nestedMap;
+
+            function setMap(address _addr, uint _i, bool _bool) public{
+                   nestedMap[_addr][_i] = _bool;
+            }
 
 }
