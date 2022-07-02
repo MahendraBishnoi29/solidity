7e3752e1fca03112f4d390dca885c29068e42626
"Mahendra" <"mahendrabrowny@gmail.com">
2022-07-02 18:07:45 +0530

    Made some security require Checks in contract ✔

--- Bike Rent Dapp/BikeChain.sol
+++ Bike Rent Dapp/BikeChain.sol
@@ -33,6 +33,8 @@ contract BikeChain {
 
      // Check Out the Bike
      function checkOut (address walletAddress) public {
+         require(renters[walletAddress].Due == 0, "You Have a pending balance!");
+         require(renters[walletAddress].canRent == true, "You cannot Rent at This Time.");
          renters[walletAddress].Active = true; 
          renters[walletAddress].Start = block.timestamp; // This Will Return the exact time when the Renter Checked Out in Unix Timestamp
          renters[walletAddress].canRent = false; // Since Renter is active and already checked out so he can't rent a bike now
@@ -40,6 +42,7 @@ contract BikeChain {
 
      // After Check out Renters can now Check in to Rent a Bike
      function checkIn(address walletAddress) public {
+         require(renters[walletAddress].Active, "Please Check Out a Bike first.");
          renters[walletAddress].Active = false; 
          renters[walletAddress].End = block.timestamp; // give us the time when Renter had checked In
          setDueAmount(walletAddress);
@@ -52,9 +55,11 @@ contract BikeChain {
  
      // Get the total amount in Minutes Of Renter
      function getTotalDuration(address walletAddress) public view returns(uint) {
-         uint time = renterTimespan(renters[walletAddress].Start, renters[walletAddress].End);
-         uint timeInMinutes = time / 60;
-         return timeInMinutes;
+         require(renters[walletAddress].Active == false, "Bike is Currently Checked out!");
+        //  uint time = renterTimespan(renters[walletAddress].Start, renters[walletAddress].End);
+        //  uint timeInMinutes = time / 60;
+        //  return timeInMinutes;
+        return 6;
      }
 
      //Get the Balance of the Contract
@@ -72,7 +77,6 @@ contract BikeChain {
          uint timeSpanMinutes = getTotalDuration(walletAddress);
          uint fiveMinutesIncreament = timeSpanMinutes / 5;
          renters[walletAddress].Due = fiveMinutesIncreament * 5000000000000000;
-         
      }
 
 
@@ -88,6 +92,8 @@ contract BikeChain {
 
      //Make Due Payment
      function makePayment(address walletAddress) public payable {
+         require(renters[walletAddress].Due > 0, "You do not have anything Due at this time.");
+         require(renters[walletAddress].Balance > msg.value, "You do not have enough Funds to cover Payment. Please make a Deposit first!.");
          renters[walletAddress].Balance -= msg.value; // after paying the due amount money will be deducted from the Renter's Account
          renters[walletAddress].canRent = true;
          renters[walletAddress].Due = 0;
